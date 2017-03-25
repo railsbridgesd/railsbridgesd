af8e45a79ce6d0d05ea0467bbbe3cdb949114e80
sslampa <sslampa@gmail.com>
2017-03-25 10:58:04 -0700

    Change navbar to be solid on scroll

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,2 +1,18 @@
 
 // JAVASCRIPTS
+
+// Changes navbar background color when scrolling over info image
+$(document).ready(function(){
+  var imageHeight = $("#intro").height();
+
+  // $(document).on("scroll", scrolling(imageHeight, this));
+  $(document).on("scroll", function(){
+    var currentPos = $(this).scrollTop();
+
+    if (currentPos < (imageHeight + 50)) {
+      $(".navbar-fixed-top").css({"background-color": "white"});
+    } else {
+      $(".navbar-fixed-top").css({"background-color": "transparent"});
+    }
+  });
+});
