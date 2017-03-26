7b4ef86bc00e121522060f83d7b9b61394e236aa
Stephanie Chin <stephjuechin@gmail.com>
2017-03-26 13:27:21 -0700

    Add contact and tweet placehodlers, link social media, fix navbar on scroll

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -3,13 +3,14 @@
 
 // Changes navbar background color when scrolling over info image
 $(document).ready(function(){
-  var imageHeight = $("#intro").height();
+  var imageHeight = $("#intro").innerHeight();
+  console.log(imageHeight)
 
   // $(document).on("scroll", scrolling(imageHeight, this));
   $(document).on("scroll", function(){
     var currentPos = $(this).scrollTop();
 
-    if (currentPos < (imageHeight + 50)) {
+    if (currentPos > (imageHeight)) {
       $(".navbar-fixed-top").css({"background-color": "white"});
     } else {
       $(".navbar-fixed-top").css({"background-color": "transparent"});
