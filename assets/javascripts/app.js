c828510ddf9753a6d5517c41f0bd3d6d10f38ae5
sslampa <sslampa@gmail.com>
2017-03-27 11:04:00 -0700

    Remove unnecessary comments

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -7,10 +7,6 @@ $(document).ready(function(){
   $(document).on("scroll", function(){
     var currentPos = $(this).scrollTop();
 
-      // $("li > a").css({"color": "#EB6852"});
-      // $(".navbar-fixed-top").css({"background-color": "white"});
-      // $("li > a").css({"color": "white", "font-weight": "bold"});
-      // $(".navbar-fixed-top").css({"background-color": "rgba(179, 40, 45, 0.75)"});
     if (currentPos > 590) {
       $(".navbar").addClass("scrolled");
     } else {
