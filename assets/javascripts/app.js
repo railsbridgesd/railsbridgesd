d946de1119ad7bdbd64a81a2fc5d50c0852cd965
Patrick Crowley <patrick@mokolabs.com>
2017-03-27 10:18:11 -0700

    Tweak navbar scrolling background #3

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,19 +1,16 @@
 
 // JAVASCRIPTS
 
-// Changes navbar background color when scrolling over info image
 $(document).ready(function(){
-  var imageHeight = $("#intro").innerHeight();
-  console.log(imageHeight)
 
-  // $(document).on("scroll", scrolling(imageHeight, this));
+  // Update navbar background as we scroll
   $(document).on("scroll", function(){
     var currentPos = $(this).scrollTop();
 
-    if (currentPos > (imageHeight)) {
-      $(".navbar-fixed-top").css({"background-color": "white"});
+    if (currentPos > 590) {
+      $(".navbar").addClass("scrolled");
     } else {
-      $(".navbar-fixed-top").css({"background-color": "transparent"});
+      $(".navbar").removeClass("scrolled");
     }
   });
 });
