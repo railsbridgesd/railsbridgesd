9ea74755c8359f1ef3c882fc2a75279fea2754c7
Patrick Crowley <patrick@mokolabs.com>
2017-03-27 17:22:57 -0700

    Tweak a bunch of things ;) - Add support for scrolling to content sections when clicking on navbar links - Rename .peach-wrap class to just .peach (and remove padding because we don't always need it) - Switch navbar-brand link to point to #intro so we can scroll back to top - Add #about link to About section so we can jump to it with the navbar - Update jQuery loader to use normal jQuery (not the slim version) - Change contact form button text to 'Send Email' - Tweak schedule section padding and margins - Tweak footer section padding and margins - Move navbar and footer text styles into layout (because they are so narrowly scoped) - Fix typo on id for volunteer section - Rename navigation partial to just 'navbar' (because that's what it is)

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -14,4 +14,16 @@ $(document).ready(function(){
     }
   });
 
+  // Scroll to section
+  $('.navbar a').click(function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 50
+      }, 500);
+      return false;
+    }
+  });
+  
 });
