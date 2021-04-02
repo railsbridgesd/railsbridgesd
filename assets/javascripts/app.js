3c0eb021c1e0ac8a8d46bb67bafe651a70ea3335
Patrick Crowley <patrick@mokolabs.com>
2021-04-02 09:59:08 -0700

    Remove whitespace

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -114,9 +114,9 @@ $(document).ready(function(){
 
   // Resize #header height on document load
   window.setTimeout(resizeHeader, 1000);
-  
+
   // Resize #header height when window is resized
-  $(window).resize(function() { 
+  $(window).resize(function() {
     resizeHeader();
   });
 
@@ -132,21 +132,21 @@ function resizeHeader() {
   // However, we need to make sure that the #header height also shrinks to match.
   // Otherwise, there will be unwanted whitespace between the bottom of the #header image
   // and the #intro content.
-  
+
   // Also, we want to avoid 'flash of unstyled content' behavior with the header image
   // (because it can push other site content down as it fully loads).
-  
+
   // To prevent this, we can hard-code values for min and max height on the #header.
   // But, when we hard-code the values, but it looks bad when the viewport width shrinks,
   // and the header image scales down responsively.
-  
+
   // But, since this only works at certain viewport widths, so we want to dynamically
   // set the min and max height to match the current height of our responsive
   // header image whenever the page is resized.
 
   // Grab height of first header image
   var header_image_height = $("#header .item").first().height();
-  
+
   // Set #header min and max height if #header image is smaller than 550 pixels high
   if (header_image_height < 550) {
     $('#header').css('min-height', header_image_height);
